c6d25ae63ec2d901850c15e2ae2161294c61be80
Nik Eugster <mail@nikeugster.ch>
2021-05-24 15:41:21 +0200

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,22 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  resources :arts do
+    resources :bookings, only: [:create, :new]
+  end
+
+  get 'dashboard', to: 'pages#dashboard', as: :dashboard
+
+#  / GET pages home
+
+#   /arts/new GET arts  new
+#   /arts POST  arts  create
+#   /arts GET arts  index
+#   /arts/:id/bookings/new  GET offers  new
+#   /arts/:id/bookings  POST  offers  create
+#   /users/:id/bookings GET users index
+#   /users/:id/bookings/:id GET users show
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
