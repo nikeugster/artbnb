de69a04b607ce7887e4c8ee18ccaba10e6468f19
Nik Eugster <mail@nikeugster.ch>
2021-05-24 16:40:30 +0200

    added valdidations

--- app/models/art.rb
+++ app/models/art.rb
@@ -1,3 +1,6 @@
 class Art < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+
+  validates_presence_of :artist, :description
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :arts, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  validates_presence_of :first_name, :last_name
 end
