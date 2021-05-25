b81f42d59a3e6c1b33a80e7e2a43d99370e6725a
Nik Eugster <mail@nikeugster.ch>
2021-05-25 13:23:39 +0200

    erease useless lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,4 @@ class User < ApplicationRecord
 
   has_many :arts, dependent: :destroy
   has_many :bookings, dependent: :destroy
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
 end
