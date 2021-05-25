02849d81c4f88c55a72247ba0659662f05ff1d73
Dominik Dewitz <dewitzdominik@kabelmail.de>
2021-05-25 14:53:16 +0200

    Add seed algorithm and faker gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_100552) do
+ActiveRecord::Schema.define(version: 2021_05_25_123251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_05_25_100552) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "price"
     t.string "title"
+    t.string "picture"
     t.index ["user_id"], name: "index_arts_on_user_id"
   end
 
